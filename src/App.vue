be3e05416304952a84f0d62b869b6a17d46e3873
syr <2226598707@qq.com>
2026-01-07 13:43:35 +0800

    基础注册登录页面

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,10 @@
 <script setup>
-  import { useRoute,useRouter } from 'vue-router';
-  import { useUserStore,useCounterStore } from '@/stores';
-  const route = new useRoute()
-  const router = new useRouter()
-
-  const goList = ()=>{
-    router.push('/list')
-    console.log(router,route);
-    
-  }
-
-  const userStore = useUserStore()
-  const counterStore = useCounterStore()
 </script>
 
 
 <template>
-  <div>
-    <hr>
-    <router-view></router-view>
-
-
-
-
-    <hr>
-    <hr>
-    <hr>
-    我是APP 
-    <el-button @click="$router.push('./home')">跳首页</el-button>
-    <el-button @click="goList">跳列表页</el-button>
-
-    <p>{{ userStore.token }}</p>
-    <el-button @click="userStore.setToken('asdasdasdasd')">登录</el-button>
-    <el-button @click="userStore.removeToken()">退出</el-button>
-    <hr>
-
-    {{ counterStore.count }}
-
-    <el-button @click="counterStore.add(2)">加</el-button>
+  <div>    
+    <router-view></router-view> 
   </div>
 </template>
 
