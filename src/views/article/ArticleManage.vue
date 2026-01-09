7374c0e3982966c2eb3fc01b8b664dd0bac6cb4e
GoldenHD <2226598707@qq.com>
2026-01-09 23:08:05 +0800

    文章管理搜索逻辑

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -6,28 +6,63 @@
     import {formatTime} from '@/utils/format.js'
     const articleList = ref([])//文章列表
     const total = ref(0)//总条数
-        //编辑逻辑
-    const onEditArticle = (row)=>{
-        console.log(row)
-    }
-        //删除逻辑
-    const onDelArticle = (row)=>{
-        console.log(row)
-    }
+    const loading = ref(false)
+   
     //定义请求参数对象
     const params= ref({
-        pagenum:1,
-        pagesize:5,
+        pagenum:1,//当前页
+        pagesize:5,//当前生效的每页条数
         cate_id:'',
         state:''
     })
-
+    //基于params参数，获取文章列表
     const getArticleList = async () =>{
+        loading.value = true
         const res = await artGetListService(params.value)
         articleList.value = res.data.data
         total.value = res.data.total
+
+        loading.value = false
     }
     getArticleList()
+    //编辑逻辑
+    const onEditArticle = (row)=>{
+        console.log(row)
+    }
+    //删除逻辑
+    const onDelArticle = (row)=>{
+        console.log(row)
+    }
+
+    //处理分页逻辑
+    const onSizeChange = (size)=>{
+        //如果每页条数变化了，原本正在访问的当前页意义不大，数据大概率已不再原来的那一页
+        params.value.pagenum = 1
+        params.value.pagesize = size
+
+        getArticleList()
+
+    }
+    const onCurrentChange = (page)=>{
+        params.value.pagenum = page
+
+        getArticleList()
+
+    }
+  
+
+    //处理搜索逻辑 => 按照最新条件重新检索，从第一页开始展示
+    const onSearch = ()=>{
+        params.value.pagenum = 1
+        getArticleList()
+    }
+
+    const onReset = ()=>{
+        params.value.cate_id = ''
+        params.value.state= ''
+        params.value.pagenum = 1
+
+    }
 </script>
 
 
@@ -51,13 +86,13 @@
 
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary">搜索</el-button>
-                    <el-button>重置</el-button>
+                    <el-button type="primary" @click="onSearch">搜索</el-button>
+                    <el-button @click="onReset">重置</el-button>
                 </el-form-item>
             </el-form>
 
             <!-- 表格区域 -->
-             <el-table :data="articleList">
+             <el-table :data="articleList" v-loading="loading">
                 <el-table-column label="文章标题" prop="title" >
                     <template #default="{row}">
                         <el-link type="primary" :underline="false">{{ row.title }}</el-link>
@@ -79,6 +114,21 @@
                     </template>
                 </el-table-column>
              </el-table>
+
+             <!-- 分页 -->
+              <el-pagination
+                v-model:current-page="params.pagenum"              
+                v-model:page-size="params.pagesize"
+                :page-sizes="[2,3,5,10]"
+                :background="true"
+                layout="jumper,total,sizes,prev,pager,next"
+                :total="total"
+                @size-change="onSizeChange"
+                @current-change="onCurrentChange"
+                style="margin-top:20px; justify-content: flex-end;"
+              />
+                
+              
     </page-container>     
 </template>
 
